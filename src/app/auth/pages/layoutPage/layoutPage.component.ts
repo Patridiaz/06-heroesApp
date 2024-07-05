b989645ca8e7066f2100669be0ec103455cc4178
Informática <informatica03@dem.local>
2024-07-05 16:45:47 -0400

    heroeApp

---
+++ src/app/auth/pages/layoutPage/layoutPage.component.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-page',
+  templateUrl: './layoutPage.component.html',
+  styles: ``
+
+})
+export class LayoutPageComponent { }
